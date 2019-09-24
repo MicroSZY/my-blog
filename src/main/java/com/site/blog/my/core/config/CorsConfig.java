d7d32175426cc02b817dec146f691545285f2b7c
szy <853323110@qq.com>
2019-09-24 21:02:20 +0800

    解决跨域问题

--- src/main/java/com/site/blog/my/core/config/CorsConfig.java
+++ src/main/java/com/site/blog/my/core/config/CorsConfig.java
@@ -14,14 +14,15 @@ public class CorsConfig {
     public FilterRegistrationBean corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
         // 设置域名你要允许的网站，如果全允许则设为 *
         config.addAllowedOrigin("*");
+        // 是否发送Cookie信息
+        config.setAllowCredentials(true);
         // 如果要限制 HEADER 或 METHOD 请自行更改
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         // 暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
-        config.addExposedHeader("*");
+        //config.addExposedHeader("*");
         // 添加映射路径
         source.registerCorsConfiguration("/**", config);
         FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
